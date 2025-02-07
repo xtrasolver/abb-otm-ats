977314c995c953666fae6b9e2c0afb2d210f6f1d
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2025-02-07 19:03:05 +0100

    Model with internal setters

--- Model/AutomaticTransferSwitch.cs
+++ Model/AutomaticTransferSwitch.cs
@@ -1,63 +1,63 @@
 public class AutomaticTransferSwitch
 {
     // Switch statuses
-    public SwitchStatus Switch1Status { get; set; }
-    public SwitchStatus Switch2Status { get; set; }
+    public SwitchStatus Switch1Status { get; internal set; }
+    public SwitchStatus Switch2Status { get; internal set; }
 
     // Voltage readings (now using ushort for voltage values)
-    public int Line1Phase1Voltage { get; set; }
-    public int Line1Phase2Voltage { get; set; }
-    public int Line1Phase3Voltage { get; set; }
-    public int Line2Phase1Voltage { get; set; }
-    public int Line2Phase2Voltage { get; set; }
-    public int Line2Phase3Voltage { get; set; }
+    public int Line1Phase1Voltage { get; internal set; }
+    public int Line1Phase2Voltage { get; internal set; }
+    public int Line1Phase3Voltage { get; internal set; }
+    public int Line2Phase1Voltage { get; internal set; }
+    public int Line2Phase2Voltage { get; internal set; }
+    public int Line2Phase3Voltage { get; internal set; }
 
     // Line status
-    public LineStatus Line1Status { get; set; }
-    public LineStatus Line2Status { get; set; }
+    public LineStatus Line1Status { get; internal set; }
+    public LineStatus Line2Status { get; internal set; }
 
     // Rated voltage
-    public RatedVoltage RatedVoltage { get; set; }
+    public RatedVoltage RatedVoltage { get; internal set; }
 
     // Voltage thresholds
-    public int UndervoltageThresholdPercent { get; set; }
-    public int OvervoltageThresholdPercent { get; set; }
+    public int UndervoltageThresholdPercent { get; internal set; }
+    public int OvervoltageThresholdPercent { get; internal set; }
 
     // Delays
-    public int TransferDelaySeconds { get; set; }
-    public int ReturnDelaySeconds { get; set; }
+    public int TransferDelaySeconds { get; internal set; }
+    public int ReturnDelaySeconds { get; internal set; }
 
     // Modbus communication settings
-    public int ModbusAddress { get; set; }
-    public int ModbusBaudRate { get; set; }
-    public ModbusParity ModbusParityCheck { get; set; }
-    public ModbusStopBits ModbusStopBits { get; set; }
+    public int ModbusAddress { get; internal set; }
+    public int ModbusBaudRate { get; internal set; }
+    public ModbusParity ModbusParityCheck { get; internal set; }
+    public ModbusStopBits ModbusStopBits { get; internal set; }
 
     // Device working mode
-    public DeviceWorkingMode DeviceWorkingMode { get; set; }
+    public DeviceWorkingMode DeviceWorkingMode { get; internal set; }
 
     // Line priority and operation mode
-    public LinePriority LinePriority { get; set; }
-    public EmergencyOffStatus EmergencyOffStatus { get; set; }
+    public LinePriority LinePriority { get; internal set; }
+    public EmergencyOffStatus EmergencyOffStatus { get; internal set; }
 
     // Operational counters and alarms
-    public int OperationCounter { get; set; }
-    public Alarm PresentAlarm { get; set; }
-    public Alarm LastAlarm { get; set; }
+    public int OperationCounter { get; internal set; }
+    public Alarm PresentAlarm { get; internal set; }
+    public Alarm LastAlarm { get; internal set; }
 
     // Software version
-    public string SoftwareVersion { get; set; }
+    public string SoftwareVersion { get; internal set; }
 
     // Phases and frequency
-    public Phases Phases { get; set; }
-    public RatedFrequency RatedFrequency { get; set; }
+    public Phases Phases { get; internal set; }
+    public RatedFrequency RatedFrequency { get; internal set; }
 
     // Generator related settings
-    public int GeneratorStopDelaySeconds { get; set; }
-    public GeneratorUsage GeneratorUsage { get; set; }
- 
+    public int GeneratorStopDelaySeconds { get; internal set; }
+    public GeneratorUsage GeneratorUsage { get; internal set; }
+    
     // Remote control settings
-    public OperatingMode OperatingMode { get; set; }
+    public OperatingMode OperatingMode { get; internal set; }
 
     public AutomaticTransferSwitch()
     {
